4e4dc9c45f06c38872d3161fe4d885a887368c7c
Daniella Stensland <dellasten25@gmail.com>
2026-03-05 15:22:05 +0100

    task A

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -3,14 +3,20 @@ package no.hvl.dat110.messages;
 public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
+	private String topic;
 
-    public CreateTopicMsg(String user, String topic) {
+	public CreateTopicMsg(String user, String topic) {
+		super(MessageType.CREATETOPIC, user);
+		this.topic = topic;
+	}
 
-    }
+	// Superclass only has getters, running on the assumption that one cannot update the topic of an already published message
+	public String getTopic() {
+		return topic;
+	}
 
-	// TODO: 
-	// Implement object variables - a topic is required
-	
-	// Complete the constructor, get/set-methods, and toString method
-    // as described in the project text	
+	@Override
+	public String toString() {
+		return "Message [type=" + super.getType() + ", user=" + super.getUser() + ", topic=" + topic +"]";
+	}
 }
